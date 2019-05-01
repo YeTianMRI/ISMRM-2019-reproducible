9061f212792ac819b0d2f4b37a2e1f784d3512e1
YeTianMRI <phye1988@gmail.com>
2019-05-01 13:55:19 -0600

    Add files via upload

--- Recon_Ye.m
+++ Recon_Ye.m
@@ -24,7 +24,7 @@ ky = trajectory(:,:,2);
 clear trajectory
 
 para.setting.plot = 0;
-para.setting.ifGPU = 0;
+para.setting.ifGPU = 1;
 %% Reconstruction of reference
 % initialize NUFFT
 Data.kSpace = kSpace;
