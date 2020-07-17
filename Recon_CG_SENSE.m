043710b330ec5767ec5c78c8250f31ff0cf28aaf
YeTianMRI <phye1988@gmail.com>
2020-07-17 10:43:49 -0700

    adjust

--- Recon_CG_SENSE.m
+++ Recon_CG_SENSE.m
@@ -38,6 +38,8 @@ para.setting.sens_map = 'adaptive';
 %% Load Brain data
 rawdata_real = h5read('rawdata_brain_radial_96proj_12ch.h5','/rawdata');
 trajectory   = h5read('rawdata_brain_radial_96proj_12ch.h5','/trajectory');
+k_size = size(trajectory, 2);
+im_size = max(trajectory(:)) * 2;
 
 % modify data format to fit our reconstruction pipeline
 kSpace = rawdata_real.r+1i*rawdata_real.i;
@@ -45,8 +47,8 @@ clear rawdata_real;
 kSpace = permute(kSpace,[3,2,4,1]);
 kSpace = kSpace*10^8; % scale up
 trajectory = permute(trajectory,[2,1,3]);
-kx = trajectory(:,:,1);
-ky = trajectory(:,:,2);
+kx = trajectory(:,:,1); kx = kx / im_size * k_size;
+ky = trajectory(:,:,2); ky = ky / im_size * k_size;
 clear trajectory
 
 %% Reconstruction of reference
@@ -63,6 +65,7 @@ Data.first_est = sum(Data.first_est,4);
 % parameters
 para.Recon.noi = 200;
 Image_96_rays = CG_SENSE(Data,para);
+Image_96_rays = crop_half_FOV(Image_96_rays, [im_size, im_size]);
 Image_reference = Image_96_rays(:,:,end);
 
 %% Conjugate Gradient NUFFT 48 rays
@@ -78,6 +81,7 @@ Data.first_est = sum(Data.first_est,4);
 % parameters
 para.Recon.noi = 100;
 [Image_48_rays,para_48_rays] = CG_SENSE(Data,para);
+Image_48_rays = crop_half_FOV(Image_48_rays, [im_size, im_size]);
 
 %% Conjugate Gradient NUFFT 32 rays
 % initialize NUFFT
@@ -92,7 +96,7 @@ Data.first_est = sum(Data.first_est,4);
 % parameters
 para.Recon.noi = 100;
 [Image_32_rays,para_32_rays] = CG_SENSE(Data,para);
-
+Image_32_rays = crop_half_FOV(Image_32_rays, [im_size, im_size]);
 
 %% Conjugate Gradient NUFFT 24 rays
 % initialize NUFFT
@@ -107,9 +111,9 @@ Data.first_est = sum(Data.first_est,4);
 % parameters
 para.Recon.noi = 100;
 [Image_24_rays,para_24_rays] = CG_SENSE(Data,para);
+Image_24_rays = crop_half_FOV(Image_24_rays, [im_size, im_size]);
 
 %% Figure 4
-
 difference_48_rays = abs(Image_48_rays - Image_reference)./abs(Image_reference);
 difference_32_rays = abs(Image_32_rays - Image_reference)./abs(Image_reference);
 difference_24_rays = abs(Image_24_rays - Image_reference)./abs(Image_reference);
@@ -119,27 +123,33 @@ difference_32_rays = log10(squeeze(sum(sum(difference_32_rays))));
 difference_24_rays = log10(squeeze(sum(sum(difference_24_rays))));
 
 figure
-plot([difference_48_rays,difference_32_rays,difference_24_rays])
+plot([difference_48_rays,difference_32_rays,difference_24_rays], 'LineWidth', 2)
 xlabel 'number of iterations'
 ylabel 'log_{10}(difference)'
 legend('R = 2','R = 3','R = 4')
-suptitle 'Corresponding to Figure 4 of Ref. [1]'
+title 'Corresponding to Figure 4 of Ref. [1]'
+set(gca, 'FontSize', 18)
+
 %% single coil recon
 Data.kSpace = kSpace(:,:,:,1);
 Data.N = NUFFT.init(kx,ky,2,[6,6]);
 Image_single_coil_96_rays = flipud(NUFFT.NUFFT_adj(Data.kSpace,Data.N));
+Image_single_coil_96_rays = crop_half_FOV(Image_single_coil_96_rays, [im_size, im_size]);
 
 Data.kSpace = kSpace(:,1:2:end,:,1);
 Data.N = NUFFT.init(kx(:,1:2:end),ky(:,1:2:end),2,[6,6]);
 Image_single_coil_48_rays = flipud(NUFFT.NUFFT_adj(Data.kSpace,Data.N));
+Image_single_coil_48_rays = crop_half_FOV(Image_single_coil_48_rays, [im_size, im_size]);
 
 Data.kSpace = kSpace(:,1:3:end,:,1);
 Data.N = NUFFT.init(kx(:,1:3:end),ky(:,1:3:end),2,[6,6]);
 Image_single_coil_32_rays = flipud(NUFFT.NUFFT_adj(Data.kSpace,Data.N));
+Image_single_coil_32_rays = crop_half_FOV(Image_single_coil_32_rays, [im_size, im_size]);
 
 Data.kSpace = kSpace(:,1:4:end,:,1);
 Data.N = NUFFT.init(kx(:,1:4:end),ky(:,1:4:end),2,[6,6]);
 Image_single_coil_24_rays = flipud(NUFFT.NUFFT_adj(Data.kSpace,Data.N));
+Image_single_coil_24_rays = crop_half_FOV(Image_single_coil_24_rays, [im_size, im_size]);
 
 
 %% Figure 5
@@ -171,11 +181,13 @@ subplot(4,3,12)
 imagesc(abs(Image_24_rays(:,:,50))),axis image,axis off, title 'Final iteration = 50'
 
 suptitle 'Corresponding to Figure 5 of Ref. [1]'
-
+set(gca, 'FontSize', 18)
 
 %% Load Cardiac Data
 rawdata_real = h5read('rawdata_heart_radial_55proj_34ch.h5','/rawdata');
 trajectory   = h5read('rawdata_heart_radial_55proj_34ch.h5','/trajectory');
+k_size = size(trajectory, 2);
+im_size = round(max(trajectory(:)) * 2);
 
 % modify data format to fit our reconstruction pipeline
 kSpace = rawdata_real.r+1i*rawdata_real.i;
@@ -183,8 +195,8 @@ clear rawdata_real;
 kSpace = permute(kSpace,[3,2,4,1]);
 kSpace = kSpace*10^8; % scale up
 trajectory = permute(trajectory,[2,1,3]);
-kx = trajectory(:,:,1);
-ky = trajectory(:,:,2);
+kx = trajectory(:,:,1); kx = kx / im_size * k_size;
+ky = trajectory(:,:,2); ky = ky / im_size * k_size;
 clear trajectory
 
 para.Recon.noi = 100;
@@ -199,6 +211,7 @@ Data.first_est = sum(Data.first_est,4);
 
 Image_55_rays = CG_SENSE(Data,para);
 Image_55_rays = abs(Image_55_rays(:,:,end));
+Image_55_rays = crop_half_FOV(Image_55_rays, [im_size, im_size]);
 
 %% 33 projection
 Data.kSpace = kSpace(:,1:33,:,:);
@@ -210,6 +223,7 @@ Data.first_est = sum(Data.first_est,4);
 
 Image_33_rays = CG_SENSE(Data,para);
 Image_33_rays = abs(Image_33_rays(:,:,end));
+Image_33_rays = crop_half_FOV(Image_33_rays, [im_size, im_size]);
 
 %% 22 projection
 Data.kSpace = kSpace(:,1:22,:,:);
@@ -221,6 +235,7 @@ Data.first_est = sum(Data.first_est,4);
 
 Image_22_rays = CG_SENSE(Data,para);
 Image_22_rays = abs(Image_22_rays(:,:,end));
+Image_22_rays = crop_half_FOV(Image_22_rays, [im_size, im_size]);
 
 %% 11 projection
 Data.kSpace = kSpace(:,1:11,:,:);
@@ -232,6 +247,7 @@ Data.first_est = sum(Data.first_est,4);
 
 Image_11_rays = CG_SENSE(Data,para);
 Image_11_rays = abs(Image_11_rays(:,:,end));
+Image_11_rays = crop_half_FOV(Image_11_rays, [im_size, im_size]);
 
 %% Figure 6
 figure
@@ -246,3 +262,4 @@ subplot(1,4,4)
 imagesc(Image_11_rays),axis image,axis off, title '11 projections'
 
 suptitle 'Corresponding to Figure 6 of Ref. [1]'
+set(gca, 'FontSize', 18)
